b1fcf7b9d2ce334785479af0e50849573d5e8eab
Lydia Lim <45248088+lydialimlh@users.noreply.github.com>
2023-12-20 13:28:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = new Member();
+    member.name = 'John Doe';
+    member.skills.push('coding');
+    member.skills.push('swimming');
+    member.skills.push('singing');
+    return member;
+}
